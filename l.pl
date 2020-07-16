aab41a311b584f1f075f897d78ce438282955a2b
liuqian <netshow-vip@sohu.com>
2020-07-16 21:58:48 +0800

    init files

--- l.pl
+++ l.pl
@@ -1,1 +1,17 @@
-print  "xxxxx";
+use strict;
+use warnings;
+use GD::Simple;
+ 
+# create a new image (width, height)
+my $img = GD::Simple->new(200, 100);
+
+# draw a red rectangle with blue borders
+$img->bgcolor('red');
+$img->fgcolor('blue');
+$img->rectangle(10, 10, 50, 50); # (top_left_x, top_left_y, bottom_right_x, bottom_right_y)
+                                 # ($x1, $y1, $x2, $y2)
+
+# convert into png data
+open my $out, '>', 'img.png' or die;
+binmode $out;
+print $out $img->png;
